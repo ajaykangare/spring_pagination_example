fc76087ecffe332e94681c72855f13844f210a11
Mindstix <ajaykangare1993@gmail.com>
2018-03-08 20:14:17 +0530

    spring pagination

---
+++ src/main/java/com/example/demo/SpringPaginationSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaginationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaginationSampleApplication.class, args);
+	}
+}
